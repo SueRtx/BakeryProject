eedbbdf0d758e72e6dea0c7a079d2437be525943
Sue Roberts <suerobertstx@yahoo.com>
2022-06-30 17:46:12 -0500

    Pass: BreadRegularPrice Test2

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -6,11 +6,29 @@ namespace Bakery
   public class Bread
   {
     public int BreadOrder { get; set; }
+    public int BreadRegPrice { get; set; }
     public int BreadOrderTotal { get; set; }
     public Bread  (int breadOrder)
     {
       BreadOrder = breadOrder;
+      BreadRegPrice = 0;
       BreadOrderTotal = 0;
     }
+
+    public int BreadRegularPrice()
+    {
+    for (int i = 0; i < BreadOrder; i++)
+    {
+      BreadRegPrice += 5;
+    }
+    return BreadRegPrice;
+    }
+
+
+
+
+
+
+
   }
 }
