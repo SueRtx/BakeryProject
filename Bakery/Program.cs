4ec28366e8fec1f30cc07d527a37f83d9e3e00fd
Sue Roberts <suerobertstx@yahoo.com>
2022-07-01 00:25:49 -0500

    Add colors to output.

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -5,33 +5,40 @@ public class Program
 {
   public static void Main()
   {
+    Console.ForegroundColor = ConsoleColor.Magenta;
     Console.WriteLine(@"
-    Sunny Bakery
-
-    ------ Menu ------
-
-    Bread
-    1 for $ 5
-    Buy 2 Get One FREE
-
-    Pastry
-    1 for $ 2
-    3 for $ 5
-    4 for $ 7
-    -------------------");
-
+        Sunny Bakery
+    -------- Menu --------
+    |                    |
+    |      *Bread*       |
+    |     1 for $ 5      |
+    | Buy 2 Get One FREE |
+    |    (3 FOR $ 10)    |
+    |                    |
+    |      *Pastry*      |
+    |     1 for $ 2      |
+    |     3 for $ 5      |
+    |     4 for $ 7      |
+    |     6 FOR $ 10     |
+    ----------------------");
+    
     SkipMenu();
     static void SkipMenu()
     {
-      Console.WriteLine("How Many Bread?");
+      Console.ForegroundColor = ConsoleColor.Blue;
+      Console.WriteLine("How many bread would you like to order?");
       int breadOrder = int.Parse(Console.ReadLine());
-      Console.WriteLine("How Many Pastries?");
+      Console.WriteLine("How Many Pastries would you like to order?");
       int pastryOrder = int.Parse(Console.ReadLine());
       Bread newBread = new Bread (breadOrder);
       newBread.BreadPrice();
       Pastry newPastry = new Pastry (pastryOrder);
       newPastry.PastryPrice();
-      Console.WriteLine("Total:" + (newBread.BreadOrderTotal + newPastry.PastryOrderTotal));
+      Console.ForegroundColor = ConsoleColor.Green;
+      Console.WriteLine("Bread :" + " " + breadOrder + " " + "for $" + newBread.BreadOrderTotal);
+      Console.WriteLine("Pastry :" + " " + pastryOrder + " " + "for $" + newPastry.PastryOrderTotal);
+      Console.ForegroundColor = ConsoleColor.Yellow;
+      Console.WriteLine("Total: $" + (newBread.BreadOrderTotal + newPastry.PastryOrderTotal));
     }
   }
 }
